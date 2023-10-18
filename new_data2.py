78581d00eda5e91602fd485420d1d897073f87f1
safaka123 <45589563+safaka123@users.noreply.github.com>
2023-10-18 14:57:43 +0300

    Update new_data2.py

--- new_data2.py
+++ new_data2.py
@@ -4,9 +4,12 @@ from keras.models import load_model
 import numpy as np
 import pandas as pd
 
+#Kütüphanelerden sonra ilk satırda modelin yüklenmesi gerçekleşir
+
 loaded_model = load_model("your_model.h5")
 
-# Defining and preprocessing
+
+# Yeni verinin tanımlanması
 new_data = {
     'Reading ID': [0],
     'Humidity': [41.5],
@@ -16,16 +19,20 @@ new_data = {
     'eCO2': [5898],
 }
 
+
+#Pandas DataFrame yardımıyla yukarıdaki veri modelin anlayacağı dile çevrilir
 df = pd.DataFrame(new_data)
 
+#Verisetinin işlem sonucu (Array içinde array olarak düşünülebilir. 0, 1 ya da 2 olmak üzere
+# yüzdelik sonuçlar elde edilir
 predictions = loaded_model.predict(df)
 print(predictions)
 
-
-
-#veri = predictions.tolist()
+#Array içinde array olarak gelen sonuçlar bir listeye dönüştürülür ve bu sayede index olarak alınabilir
 flat_list = predictions.flatten().tolist()
 
+
+#Index değişkenleri ile verilere erişilir ve okuması daha kolay olduğundan yüzdelik formatta gösterilir
 print(flat_list)
 print("%" + str(flat_list[0] * 100))
 print("%" + str(flat_list[1] * 100))
